d2949914c882fce7dc18be25f0ac4318eca82955
Danielle Luz <daniln2209@gmail.com>
2023-11-02 23:36:54 -0300

    fix: remove return type from get pdf content method

--- src/services/pdf.services.py
+++ src/services/pdf.services.py
@@ -2,7 +2,7 @@ import PyPDF2
 
 
 class TextFileReader:
-    def __init__(self, file_path) -> None:
+    def __init__(self, file_path):
         self.file = open(file_path, "rb")
 
     def get_pdf_content_as_text(self):
